tower-rs/tower

commit 92f4a0cb729a82df01086fa3c8e0e4dd80d36947

Rename tower-util -> tower (#197)

Cargo.toml
@@ -15,6 +15,5 @@ members = [
   "tower-service",
   "tower-service-util",
   "tower-timeout",
-  "tower-util",
   "tower-watch",
 ]

tower-util/Cargo.toml
@@ -1,13 +1,0 @@
-[package]
-name = "tower-util"
-version = "0.1.0"
-authors = ["Carl Lerche <me@carllerche.com>"]
-publish = false
-
-[dependencies]
-futures = "0.1"
-tower-service = "0.2.0"
-tower-service-util = { version = "0.1.0", path = "../tower-service-util", features = ["io", "either"] }
-
-[dev-dependencies]
-tokio-mock-task = "0.1"

tower/Cargo.toml
@@ -13,11 +13,16 @@ clients and servers.
 categories = ["asynchronous", "network-programming"]
 keywords = ["io", "async", "non-blocking", "futures", "service"]
 
+[dependencies]
+futures = "0.1"
+tower-service = "0.2.0"
+tower-service-util = { version = "0.1.0", path = "../tower-service-util", features = ["io", "either"] }
+
 [dev-dependencies]
 tower-service = "0.2.0"
-tower-util = { version = "0.1", path = "../tower-util" }
 futures = "0.1"
 log = "0.4.1"
 env_logger = { version = "0.5.3", default-features = false }
 tokio-timer = "0.1"
 futures-cpupool = "0.1"
+tokio-mock-task = "0.1"

azure-pipelines.yml
@@ -13,20 +13,20 @@ jobs:
     displayName: Test
     vmImage: ubuntu-16.04
     crates:
-      - balance
-      - buffer
-      - discover
-      - filter
-      - in-flight-limit
-      - layer
-      - mock
-      - rate-limit
-      - reconnect
-      - retry
-      - service
-      - timeout
-      - util
-      - watch
+      - tower-balance
+      - tower-buffer
+      - tower-discover
+      - tower-filter
+      - tower-in-flight-limit
+      - tower-layer
+      - tower-mock
+      - tower-rate-limit
+      - tower-reconnect
+      - tower-retry
+      - tower-service
+      - tower-timeout
+      - tower-watch
+      - tower
 
 - template: ci/azure-deploy-docs.yml
   parameters:

ci/azure-test-stable.yml
@@ -13,5 +13,5 @@ jobs:
 
   - ${{ each crate in parameters.crates }}:
     - script: cargo test
-      displayName: cargo test -p tower-${{ crate }}
-      workingDirectory: $(Build.SourcesDirectory)/tower-${{ crate }}
+      displayName: cargo test -p ${{ crate }}
+      workingDirectory: $(Build.SourcesDirectory)/${{ crate }}

tower-util/src/lib.rs
@@ -1,18 +1,0 @@
-//! Various utility types and functions that are generally with Tower.
-
-#[macro_use]
-extern crate futures;
-#[cfg(test)]
-extern crate tokio_mock_task;
-extern crate tower_service;
-extern crate tower_service_util;
-
-pub mod ext;
-
-pub use ext::ServiceExt;
-pub use tower_service_util::BoxService;
-pub use tower_service_util::EitherService;
-pub use tower_service_util::MakeConnection;
-pub use tower_service_util::MakeService;
-pub use tower_service_util::OptionService;
-pub use tower_service_util::ServiceFn;

tower/examples/channel_service.rs
@@ -12,16 +12,16 @@
 extern crate futures;
 extern crate futures_cpupool;
 extern crate tokio_timer;
+extern crate tower;
 extern crate tower_service;
-extern crate tower_util;
 
 #[macro_use]
 extern crate log;
 
 extern crate env_logger;
 
+use tower::{MakeService, ServiceExt};
 use tower_service::Service;
-use tower_util::{MakeService, ServiceExt};
 
 use futures::future::{Executor, FutureResult};
 use futures::sync::{mpsc, oneshot};

tower-rs/tower-hyper

commit d27288a5669fc1315a0ee26ffff62a1eed782dd6

Update to new Tower Repository Structure (#13)
This resolves build issues introduced by https://github.com/tower-rs/tower/pull/197.

Cargo.toml
@@ -14,7 +14,7 @@ tokio-buf = "0.1"
 tokio-executor = "0.1"
 tower-service = "0.2"
 tower-retry = { git = "https://github.com/tower-rs/tower", optional = true }
-tower-util = { git = "https://github.com/tower-rs/tower" }
+tower = { git = "https://github.com/tower-rs/tower" }
 tower-http-service = { git = "https://github.com/tower-rs/tower-http" }
 
 [dev-dependencies]

examples/conn_client.rs
@@ -4,11 +4,11 @@ use hyper::client::conn::Builder;
 use hyper::client::connect::{Destination, HttpConnector};
 use hyper::rt;
 use hyper::Body;
+use tower::MakeService;
 use tower_buffer::Buffer;
 use tower_hyper::client::Connect;
-use tower_service::Service;
-use tower_util::MakeService;
 use tower_hyper::util::Connector;
+use tower_service::Service;
 
 fn main() {
     pretty_env_logger::init();
@@ -20,10 +20,7 @@ fn main() {
         hyper
             .make_service(dst)
             .map_err(|err| eprintln!("Connect Error {:?}", err))
-            .and_then(|conn| {
-                Buffer::new(conn, 1)
-                    .map_err(|_| panic!("Unable to spawn!"))
-            })
+            .and_then(|conn| Buffer::new(conn, 1).map_err(|_| panic!("Unable to spawn!")))
             .and_then(|mut conn| {
                 conn.call(Request::new(Body::empty()))
                     .map_err(|e| eprintln!("Call Error: {}", e))

examples/retry.rs
@@ -3,13 +3,13 @@ use http::{Request, Uri};
 use hyper::client::conn::Builder;
 use hyper::client::connect::{Destination, HttpConnector};
 use hyper::rt;
+use tower::MakeService;
 use tower_buffer::Buffer;
 use tower_hyper::client::Connect;
 use tower_hyper::retry::{Body, RetryPolicy};
 use tower_hyper::util::Connector;
 use tower_retry::Retry;
 use tower_service::Service;
-use tower_util::MakeService;
 
 fn main() {
     pretty_env_logger::init();

src/client/connect.rs
@@ -9,8 +9,8 @@ use std::error::Error as StdError;
 use std::fmt;
 use std::marker::PhantomData;
 use tokio_executor::DefaultExecutor;
+use tower::MakeConnection;
 use tower_service::Service;
-use tower_util::MakeConnection;
 
 /// Creates a `hyper` connection
 ///

src/server/mod.rs
@@ -6,9 +6,9 @@ use hyper::service::Service as HyperService;
 use hyper::Body;
 use hyper::{Request, Response};
 use tokio_io::{AsyncRead, AsyncWrite};
+use tower::MakeService;
 use tower_http_service::HttpService;
 use tower_service::Service;
-use tower_util::MakeService;
 
 pub use hyper::server::conn::Http;
 