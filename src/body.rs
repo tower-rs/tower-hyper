04f8e63de6df827180b59817b1aa7cd23ceead30
Lucio Franco <luciofranco14@gmail.com>
2019-05-21 10:31:19 -0400

    Update to use `hyper::Body`

--- src/body.rs
+++ src/body.rs
@@ -4,11 +4,7 @@ use futures::Poll;
 use http_body::Body as HttpBody;
 use hyper::body::Payload;
 
-/// Specialized Body that takes a `hyper::Body` and implements `tower_http::Body`.
-#[derive(Debug)]
-pub struct Body {
-    inner: hyper::Body,
-}
+pub use hyper::Body;
 
 /// Lifts a body to support `Payload`
 #[derive(Debug)]
@@ -38,53 +34,6 @@ where
     fn poll_trailers(&mut self) -> Poll<Option<hyper::HeaderMap>, Self::Error> {
         self.inner.poll_trailers()
     }
-    fn is_end_stream(&self) -> bool {
-        self.inner.is_end_stream()
-    }
-}
-
-impl From<hyper::Body> for Body {
-    fn from(inner: hyper::Body) -> Self {
-        Body { inner }
-    }
-}
-
-impl Body {
-    /// Get the inner wrapped `hyper::Body`.
-    pub fn into_inner(self) -> hyper::Body {
-        self.inner
-    }
-}
-
-impl HttpBody for Body {
-    type Data = hyper::Chunk;
-    type Error = hyper::Error;
-
-    fn poll_data(&mut self) -> Poll<Option<Self::Data>, Self::Error> {
-        self.inner.poll_data()
-    }
-
-    fn poll_trailers(&mut self) -> Poll<Option<hyper::HeaderMap>, Self::Error> {
-        self.inner.poll_trailers()
-    }
-
-    fn is_end_stream(&self) -> bool {
-        self.inner.is_end_stream()
-    }
-}
-
-impl Payload for Body {
-    type Data = hyper::Chunk;
-    type Error = hyper::Error;
-
-    fn poll_data(&mut self) -> Poll<Option<Self::Data>, Self::Error> {
-        self.inner.poll_data()
-    }
-
-    fn poll_trailers(&mut self) -> Poll<Option<hyper::HeaderMap>, Self::Error> {
-        self.inner.poll_trailers()
-    }
-
     fn is_end_stream(&self) -> bool {
         self.inner.is_end_stream()
     }
